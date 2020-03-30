c57b0ef609bc18a86afb42f55132c07135d626fa
mohammad nour itani <mohammad.nour.itani@gmail.com>
2020-03-30 17:30:37 +0300

    Add header and header style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import ShopPage from './pages/shop/shop.component'
-import HomePage from './pages/homepage/homepage.comonent';
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import ShopPage from "./pages/shop/shop.component";
+import HomePage from "./pages/homepage/homepage.comonent";
+import Header from "./components/header/header.component";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {  }
+    this.state = {};
   }
 
-  
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
   }
-
 }
 
 export default App;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assest/crown.svg";
+const Header = () => (
+  <div className="header">
+    <Link  to="/">
+      <Logo className="logo-container"></Logo>
+    </Link>
+    <div className="options">
+      <Link to="/shop" className="option">
+        SHOP
+      </Link>
+      <Link to="/shop" className="option">
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
